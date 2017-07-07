6c38ed0d405b12ce8702b25fdd03ed563c81010a
max pleaner <maxpleaner@gmaeil.com>
2017-07-07 15:31:38 -0700

    refactor tests to one-liners and add tests that the correct status codes are returned

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,3 +1,5 @@
+# See spec/helpers.rb for the definitions of the utility methods used here
+
 RSpec.describe "API" do
 
   context "POST /email" do
@@ -12,34 +14,45 @@ RSpec.describe "API" do
         body: "<h1> Your bill</h1><p> $10</p>"
       }
     end
-    let(:valid_params_string) { valid_params.to_query }
-    let(:invalid_params) { {} }
-    let(:invalid_params_string) { invalid_params.to_query }
+
+    let(:invalid_params) { Hash.new }
+
+    let(:response_to_query_with_valid_params) do
+      send_email_with_curl_and_one_off_server(valid_params)
+    end
+
+    let(:response_to_query_with_invalid_params) do
+      send_email_with_curl_and_one_off_server(invalid_params)
+    end
 
     describe "basic endpoint functionality over CURL" do
 
       context "with valid params" do
-
         it "returns a JSON-encoded object" do
-          with_running_server do |base_url|
-            endpoint = "#{base_url}/email?#{valid_params_string}"
-            response = JSON.parse `curl -X POST #{endpoint}`
-            expect(response).to be_a Hash
-          end
+          expect(response_to_query_with_valid_params[:response]).to be_a Hash
         end
-
       end
 
       context "with invalid params" do
-
         it "still returns a JSON-encoded object" do
-          with_running_server do |base_url|
-            endpoint = "#{base_url}/email?#{invalid_params_string}"
-            response = JSON.parse `curl -X POST #{endpoint}`
-            expect(response).to be_a Hash
-          end
+          expect(response_to_query_with_invalid_params[:response]).to be_a Hash
         end
+      end
 
+    end
+
+    describe "status codes" do
+
+      context "with valid params" do
+        it "returns 200" do
+          expect(response_to_query_with_valid_params[:status_code]).to eq 200
+        end
+      end
+
+      context "with invalid params" do
+        it "returns 422" do
+          expect(response_to_query_with_invalid_params[:status_code]).to eq 422
+        end
       end
 
     end
