f79c203c8ae3a824f7279d210a36d5a13df844a4
max pleaner <maxpleaner@gmaeil.com>
2017-07-07 15:31:05 -0700

    use correct method to set status code in sinatra response

--- server.rb
+++ server.rb
@@ -12,7 +12,7 @@ class Server < Sinatra::Base
 
   post '/email' do
     result = EmailProcessor.run(request)
-    status_code result[:status_code]
+    status result[:status_code]
     content_type "application/json"
     result[:response].to_json
   end
