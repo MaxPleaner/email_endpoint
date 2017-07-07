98ebe877ddf69012f9b9329c26864ca1fb6fa3fa
max pleaner <maxpleaner@gmaeil.com>
2017-07-07 15:32:39 -0700

    update tests to get status code and response from CURL. Still debugging this, since the new CURL script causes a syntax error when called from ruby for some reason (but not bash)

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -2,13 +2,12 @@ require 'socket' # from Ruby Std-lib, provides TCPServer
 
 # Runs a server in a background thread and closes it when the block is done
 # @yield [base_url]
-# @return [void]
+# @return the result of the yield
 def with_running_server(&blk)
   port = find_open_port
   thread = Thread.new { `rackup -p #{port} &> /dev/null` }
   sleep 1 # TODO: remove this
-  blk.call("http://localhost:#{port}")
-  thread.kill
+  blk.call("http://localhost:#{port}").tap { thread.kill }
 end
 
 private
@@ -18,4 +17,34 @@ private
 def find_open_port
   server = TCPServer.new('127.0.0.1', 0)
   server.addr[1].to_i.tap { server.close }
+end
+
+# Sends an email using POST /email
+# @param params [Hash] converted to a query string
+# @return [Hash] with keys:
+#   status_code (Integer)
+#   response (Hash, if the endpoint works as expected)
+def send_email_with_curl_and_one_off_server(params)
+  with_running_server do |base_url|
+    endpoint = "#{base_url}/email?#{params.to_query}"
+    curl_post_with_status_code(endpoint)
+  end
+end
+
+# @param endpoint [String] should already have params appended if there are any
+# @return Hash with keys:
+#   status_code: Integer
+#   response (Hash, if the endpoint works as expected)
+def curl_post_with_status_code(endpoint)
+  cmd = <<-SH
+    HTTP_STATUS=$(         \
+      curl                 \
+        -w "%{http_code}"  \
+        -o >(cat >&3)      \
+        '#{endpoint}'      \
+    )
+  SH
+  response = `#{cmd}`
+  status_code = `echo $HTTP_STATUS`
+  { response: response, status_code: status_code }
 end
