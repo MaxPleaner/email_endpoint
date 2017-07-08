9d6c69831a1d1d6d41fc1ef48ba1f940a5fece87
max pleaner <maxpleaner@gmaeil.com>
2017-07-08 16:17:39 -0700

    use define_singleton_method in email_provder.rb, not define_method. Also some minor other updates here

--- lib/email_provider.rb
+++ lib/email_provider.rb
@@ -1,16 +1,17 @@
 class EmailProvider
 
   # A container which components get added to dynamically.
-  # See the lib/email_providers/ folder for individual provider APIs
+  # Use {.register} to do so.
+  # See the lib/email_providers/ folder for builtin provider APIs
   Providers = {}
 
   # It isn't necessary for provider components to inherit from this protocol,
   # but it can be done so to ensure that all required methods are defined.
   class Protocol
-    # @param endpoint
+    # @param params [Hash]
     # @raise [RuntimeError], but this method is expected to be redefined.
     #   it should return a Hash with status_code (Int) and response (Hash) keys
-    def self.send_email(endpoint); raise("not implemented"); end
+    def self.send_email(params); raise("not implemented"); end
   end
 
   # @param class_or_module [Class] which implenents EmailProvider::Protocol
@@ -25,7 +26,7 @@ class EmailProvider
   # @return [EmailProvider] with #send_email delegated to the provider
   def initialize(provider_name)
     @provider = self.class::Providers[provider_name]
-    self.class.send(:define_method, :send_email, &provider.method(:send_email))
+    define_singleton_method :send_email, &provider.method(:send_email)
   end
 
 end
