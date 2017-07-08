2f169a7973e5596c8e16d5bfb27c90c166fbfd74
max pleaner <maxpleaner@gmaeil.com>
2017-07-08 16:19:15 -0700

    realize that Mechanize does not have identical method signatures for all HTTP request types. Add a case statement to the HttpClient.request method which only accepts get and post for now.

--- lib/http_client.rb
+++ lib/http_client.rb
@@ -7,14 +7,24 @@ class HttpClient
   # A shared instance of the Mechanize HTTP Client
   Agent = Mechanize.new
 
-  # @param type [Symbol] a HTTP request type e.g. :get or :post
+  # @param type [Symbol] a HTTP request type. One of :get or :post.
   # @param url [String] should already have params appended if there are any
+  # @keyword params [Hash] defaults to {}
+  # @keyword referrer [String], only used for :get
+  # @keyword headers [Hash] defaults to {}
   # @return Hash with keys:
   #   status_code: Integer
   #   response (Hash, if the endpoint works as expected)  
   # If the response isn't JSON, this will raise a JSON::ParserError
-  def self.request(type, url)
-    result = Agent.send(type, url)
+  def self.request(type, url, params: {}, referrer: nil, headers: {})
+    result = case type
+    when :get
+      Agent.send(type, url, referrer, params, headers)
+    when :post
+      Agent.send(type, url, params, headers)
+    else
+      raise(ArgumentError, "HttpClient.request can't handle type #{type}")
+    end
     response = JSON.parse result.body
     { response: response, status_code: result.code.to_i } 
   rescue Mechanize::ResponseCodeError => e
