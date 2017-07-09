bc2af3197f3be7682bdc035e028e48517ad96c88
max pleaner <maxpleaner@gmaeil.com>
2017-07-09 02:36:50 -0700

    update api spec
    add test that the html is sanitized (it isnt yet)
    
    and also one which uses the gmail gem to check that mail was
    actually delivered.
    
    This delivery tests works, though some of the fine details
    (like reading the body of the HTML without HTML injected by SendGrid,
    as well as the proper to and from names) need work

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "API" do
 
     let(:valid_params) do
       {
-        "to" => "maxpleaner@gmail.com",
+        "to" => "#{ENV.fetch("TEST_EMAIL_USERNAME")}@gmail.com",
         "to_name" => "Mr. Fake",
         "from" => "noreply@mybrightwheel.com",
         "from_name" => "Brightwheel",
@@ -19,14 +19,14 @@ RSpec.describe "API" do
     let(:invalid_params) { Hash.new }
 
     let(:response_to_query_with_valid_params) do
-      send_email_with_curl_and_one_off_server(valid_params)
+      send_email_with_local_server(valid_params)
     end
 
     let(:response_to_query_with_invalid_params) do
-      send_email_with_curl_and_one_off_server(invalid_params)
+      send_email_with_local_server(invalid_params)
     end
 
-    describe "basic endpoint functionality over CURL" do
+    describe "The endpoint returning JSON" do
 
       context "with valid params" do
         it "returns a JSON-encoded object" do
@@ -48,10 +48,55 @@ RSpec.describe "API" do
 
     end
 
+    describe "Sanitizing HTML" do
+
+      it "changes the HTML somehow" do
+        html = "<p>foo</p>"
+        expect(html).not_to eq(
+          EmailSender.sanitize_html(html)
+        )
+      end
+
+    end
+
+    describe "The endpoint sending email" do
+
+      context "with valid params" do
+        it "sends an email." do
+          with_smtp_server do |server|
+            server.fetch_unread # clear the existing inbox
+            sleep 2.5 # give inbox time to empty
+            server.fetch_unread
+            expect(server.fetch_unread.length).to eq(0) # make sure it's empty
+            result = response_to_query_with_valid_params
+            expect(result[:status_code]).to eq 202
+            sleep 2.5 # wait for email to arrive
+            messages = server.fetch_unread
+            expect(messages.length).to eq 1
+            msg = messages.shift
+            expect(msg.from_addrs[0]).to eq(valid_params["from"])
+            # expect(msg.from).to eq(valid_params["from_name"])
+            # expect(msg.to).to eq(valid_params["to_name"])
+            expect(msg.subject).to eq(valid_params["subject"])
+            expect(msg.body.to_s).to include(
+              EmailSender.sanitize_html(valid_params["body"])
+            )
+          end
+        end
+      end
+
+      context "with invalid params" do
+        it "does not send an email" do
+          response_to_query_with_invalid_params
+        end
+      end      
+
+    end
+
     describe "status codes" do
 
       context "with valid params" do
-        it "returns 200" do
+        it "returns 202" do
           expect(response_to_query_with_valid_params[:status_code]).to eq 202
         end
       end
@@ -65,4 +110,5 @@ RSpec.describe "API" do
     end
 
   end
+
 end
