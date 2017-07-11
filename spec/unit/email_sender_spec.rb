23444a5c11da941d2282230e4b20b1988a016786
max pleaner <maxpleaner@gmaeil.com>
2017-07-11 03:11:39 -0700

    write first spec for email sender

--- spec/unit/email_sender_spec.rb
+++ spec/unit/email_sender_spec.rb
@@ -1,37 +1,25 @@
-# Fake classes
-class ValidFakeProvider < EmailProvider::Protocol
-  Response = {status_code: 202, response: {}}
-  def self.send_email(params); Response; end
-end
-class InvalidFakeProvider < EmailProvider::Protocol
-end
+using TestHelpers
 
-RSpec.describe "EmailProvider" do
+RSpec.describe "EmailSender" do
+  
+  it "defines a default provider name, loaded from env vars" do
+    expect(EmailSender::DefaultProviderName.tap &not_blank!).to be_a Symbol
+  end
 
-  describe "using child classes with a protocol" do
-    it "tracks providers in a constant" do
-      expect(EmailProvider::Providers).to be_a(Hash)
+  describe ".run" do
+    it "sends an email when given params" do
+      request = double
+      provider_name = EmailSender::DefaultProviderName
+      expect(request).to receive(:payload).and_return(valid_params)
+      expect(EmailSender).to(
+        receive(:send_email).with(any_args).and_return(
+          status_code: 202, response: "foo"
+        )
+      )
+      expect(EmailSender.run request, provider_name).to include(
+        status_code: 202, response: "foo"
+      )
     end
-
-    it "allows providers to add themselves to it" do
-      EmailProvider.register(ValidFakeProvider)
-      expect(EmailProvider::Providers[:ValidFakeProvider]).to eq(ValidFakeProvider)
-    end
-
-    it "can use registered providers in #initialize" do
-      EmailProvider.register(ValidFakeProvider)
-      provider = EmailProvider.new(:ValidFakeProvider)
-      expect(provider.send_email({})).to eq ValidFakeProvider::Response
-    end
-
-    it "will raise an error if the provider doesn't respond to .send_email" do
-      EmailProvider.register(InvalidFakeProvider)
-      provider = EmailProvider.new(:InvalidFakeProvider)
-      expect do 
-        provider.send_email({})
-      end.to raise_error(EmailProvider::Protocol::MethodNotImplementedError)
-    end
-
   end
 
 end
